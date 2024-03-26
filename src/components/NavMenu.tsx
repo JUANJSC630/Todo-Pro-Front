a5ea3cc28052171871a5e3e709fa659177e00aac
Juan Saldarriaga <juan@ocmiwc.com>
2024-03-26 00:13:05 -0500

    test commit 23

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,6 +1,6 @@
 function NavMenu() {
   return (
-    <div className="w-full flex bg-white">
+    <div className="w-full flex bg-blue-500">
       <button className="w-full text-gray-400">Home</button>
       <button className="w-full text-gray-400">About</button>
       <button className="w-full text-gray-400">Contact</button>
